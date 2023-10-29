ee532aed186af3d837f33e82382783f4eaf3a82e
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-10-29 23:13:11 +0600

    Create dynamic route for doctor details page

---
+++ app/doctors/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+interface DoctorDetailsProps {
+  params: { id: string };
+}
+
+const DoctorDetails: FC<DoctorDetailsProps> = ({ params }) => {
+  return <div></div>;
+};
+
+export default DoctorDetails;
