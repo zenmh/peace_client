e2e80a1247c1849763e48d11d29c96ce42ecacf5
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-10-31 18:19:00 +0600

    Create custom textarea namde txtarea component

---
+++ components/shared/Txtarea.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Textarea } from "../ui/textarea";
+
+interface TextareaProps {}
+
+const Txtarea: FC<TextareaProps> = () => {
+  return <Textarea />;
+};
+
+export default Txtarea;
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -4,5 +4,6 @@ import Btn from "./Btn";
 import Footer from "./Footer";
 import Tooltip from "./Tooltip";
 import Inp from "./Inp";
+import Txtarea from "./Txtarea";
 
-export { ModeSwitcher, Navbar, Btn, Footer, Tooltip, Inp };
+export { ModeSwitcher, Navbar, Btn, Footer, Tooltip, Inp, Txtarea };
